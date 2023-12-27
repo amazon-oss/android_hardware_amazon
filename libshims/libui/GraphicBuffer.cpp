3df91cce047ce9743b08a4f5528a4018a91d51c9
ZeeLog <Danjel.Zammit@gmail.com>
2023-12-27 14:17:06 +0100

    amazon: libshims: Shim yet another `GraphicBuffer` constructor

--- libshims/libui/GraphicBuffer.cpp
+++ libshims/libui/GraphicBuffer.cpp
@@ -18,3 +18,21 @@ extern "C" void *_ZN7android13GraphicBufferC1Ejjij(void *thisptr,
   return _ZN7android13GraphicBufferC1EjjijNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(
       thisptr, inWidth, inHeight, inFormat, inUsage);
 }
+
+extern "C" void *_ZN7android13GraphicBufferC1EjjijjjP13native_handleb(
+    void *thisptr, const native_handle_t *handle,
+    android::GraphicBuffer::HandleWrapMethod method, uint32_t width,
+    uint32_t height, int format, uint32_t layerCount, uint64_t usage,
+    uint32_t stride);
+
+extern "C" void *_ZN7android13GraphicBufferC1EjjijjP13native_handleb(
+    void *thisptr, uint32_t inWidth, uint32_t inHeight, int inFormat,
+    uint32_t inUsage, uint32_t inStride, native_handle_t *inHandle,
+    bool keepOwnership) {
+  android::GraphicBuffer::HandleWrapMethod inMethod =
+      (keepOwnership ? android::GraphicBuffer::TAKE_HANDLE
+                     : android::GraphicBuffer::WRAP_HANDLE);
+  return _ZN7android13GraphicBufferC1EjjijjjP13native_handleb(
+      thisptr, inHandle, inMethod, inWidth, inHeight, inFormat,
+      static_cast<uint32_t>(1), static_cast<uint64_t>(inUsage), inStride);
+}
