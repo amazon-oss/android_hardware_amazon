7a204d86b1f6d8b808b571bef3acc85070d0148d
R0rt1z2 <me@r0rt1z2.com>
2023-12-29 13:28:27 +0100

    amazon: libshims: Fix ABI issues

--- libshims/libui/GraphicBufferMapper.cpp
+++ libshims/libui/GraphicBufferMapper.cpp
@@ -1,20 +1,33 @@
 #include <stdint.h>
-#include <ui/Rect.h>
 #include <ui/GraphicBufferMapper.h>
+#include <ui/Rect.h>
 
-namespace android {
-    extern "C" void _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr, int32_t* outBytesPerPixel, int32_t* outBytesPerStride);
+using android::Rect;
+using android::status_t;
 
-    extern "C" void _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr) {
-        _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(handle, usage, bounds, vaddr, nullptr, nullptr);
-    }
+extern "C" {
+status_t
+_ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(
+    void *thisptr, buffer_handle_t handle, uint32_t usage, const Rect &bounds,
+    void **vaddr, int32_t *outBytesPerPixel, int32_t *outBytesPerStride);
+
+status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(
+    void *thisptr, buffer_handle_t handle, uint32_t usage, const Rect &bounds,
+    void **vaddr) {
+  return _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(
+      thisptr, handle, usage, bounds, vaddr, nullptr, nullptr);
+}
 }
 
 extern "C" {
-    void _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handlejRKNS_4RectEP13android_ycbcr(buffer_handle_t, uint32_t, const android::Rect&, android_ycbcr*);
-    void _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handleiRKNS_4RectEP13android_ycbcr(buffer_handle_t, int, const android::Rect&, android_ycbcr*);
-
-    void _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handleiRKNS_4RectEP13android_ycbcr(buffer_handle_t handle, int usage, const android::Rect& bounds, android_ycbcr *ycbcr) {
-        _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handlejRKNS_4RectEP13android_ycbcr(handle, static_cast<uint32_t>(usage), bounds, ycbcr);
-    }
+status_t
+_ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handlejRKNS_4RectEP13android_ycbcr(
+    void *thisptr, buffer_handle_t handle, uint32_t usage, const Rect &bounds,
+    android_ycbcr *ycbcr);
+status_t _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handleiRKNS_4RectEP13android_ycbcr(
+    void *thisptr, buffer_handle_t handle, int usage, const Rect &bounds,
+    android_ycbcr *ycbcr) {
+  return _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handlejRKNS_4RectEP13android_ycbcr(
+      thisptr, handle, static_cast<uint32_t>(usage), bounds, ycbcr);
+}
 }
