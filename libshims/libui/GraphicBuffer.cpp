2f87828a67e0e0b84fb41743199e2c607200bc6a
Nolen Johnson <johnsonnolen@gmail.com>
2024-01-04 01:06:31 +0100

    amazon: libshims: Provide shim for `GraphicBuffer::lock`
    Co-authored-by: Erfan Abdi <erfangplus@gmail.com>
    Co-authored-by: Quallenauge <Hamsi2k@freenet.de>
    Co-authored-by: Sebastiano Barezzi <seba@sebaubuntu.dev>

--- libshims/libui/GraphicBuffer.cpp
+++ libshims/libui/GraphicBuffer.cpp
@@ -1,9 +1,12 @@
+#include <stdint.h>
 #include <string.h>
 #include <sys/types.h>
 
 #include <ui/GraphicBuffer.h>
+#include <utils/Errors.h>
 
 using android::PixelFormat;
+using android::status_t;
 
 extern "C" void *
 _ZN7android13GraphicBufferC1EjjijNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(
@@ -35,4 +38,15 @@ extern "C" void *_ZN7android13GraphicBufferC1EjjijjP13native_handleb(
   return _ZN7android13GraphicBufferC1EjjijjjP13native_handleb(
       thisptr, inHandle, inMethod, inWidth, inHeight, inFormat,
       static_cast<uint32_t>(1), static_cast<uint64_t>(inUsage), inStride);
+}
+
+extern "C" status_t _ZN7android13GraphicBuffer4lockEjPPvPiS3_(
+    void *thisptr, uint32_t inUsage, void **vaddr, int32_t *outBytesPerPixel,
+    int32_t *outBytesPerStride);
+
+extern "C" status_t _ZN7android13GraphicBuffer4lockEjPPv(void *thisptr,
+                                                         uint32_t inUsage,
+                                                         void **vaddr) {
+  return _ZN7android13GraphicBuffer4lockEjPPvPiS3_(thisptr, inUsage, vaddr,
+                                                   nullptr, nullptr);
 }
