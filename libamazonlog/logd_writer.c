50d90b43e650c5fb273782a0b39889835d7a096b
R0rt1z2 <me@r0rt1z2.com>
2024-07-30 18:51:15 +0200

    amazon: libamazonlog: Skip floody log tags

--- libamazonlog/logd_writer.c
+++ libamazonlog/logd_writer.c
@@ -5,21 +5,32 @@
 #define LOG_BUF_SIZE 1024
 #define NEW_TAG_SIZE 128
 
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+const char *flooding_tags[] = {"wmt_loader"};
+
 #include <log/log.h>
 
 int lab126_log_write(int bufID __unused, int prio __unused, const char *tag,
                      const char *fmt, ...) {
-  char buf[LOG_BUF_SIZE];
-  char new_tag[NEW_TAG_SIZE];
+    char buf[LOG_BUF_SIZE];
+    char new_tag[NEW_TAG_SIZE];
 
-  if (!tag)
-    tag = "";
+    if (!tag)
+        tag = "";
 
-  va_list ap;
-  va_start(ap, fmt);
-  vsnprintf(buf, LOG_BUF_SIZE, fmt, ap);
-  va_end(ap);
+    for (size_t i = 0; i < ARRAY_SIZE(flooding_tags); ++i) {
+        if (strcmp(tag, flooding_tags[i]) == 0) {
+            return 0;
+        }
+    }
 
-  snprintf(new_tag, sizeof(new_tag), "AMZ-%s", tag);
-  return __android_log_buf_write(LOG_ID_MAIN, ANDROID_LOG_DEBUG, new_tag, buf);
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(buf, LOG_BUF_SIZE, fmt, ap);
+    va_end(ap);
+
+    snprintf(new_tag, sizeof(new_tag), "AMZ-%s", tag);
+    return __android_log_buf_write(LOG_ID_MAIN, ANDROID_LOG_DEBUG, new_tag,
+                                   buf);
 }
