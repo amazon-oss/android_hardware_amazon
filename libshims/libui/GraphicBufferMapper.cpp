e4c986652e70620cd0e754c1bc66fdc470c8d41a
R0rt1z2 <me@r0rt1z2.com>
2024-12-14 22:30:33 +0100

    amazon: libshims: Shim missing `libui` symbols

--- libshims/libui/GraphicBufferMapper.cpp
+++ libshims/libui/GraphicBufferMapper.cpp
@@ -18,4 +18,17 @@ status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handleiRKNS_4RectEPPv(
 }
 }
 
+extern "C" {
+status_t
+_ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handlejRKNS_4RectEP13android_ycbcr(
+    void *thisptr, buffer_handle_t handle, uint32_t usage, const Rect &bounds,
+    android_ycbcr *ycbcr);
+status_t _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handleiRKNS_4RectEP13android_ycbcr(
+    void *thisptr, buffer_handle_t handle, int usage, const Rect &bounds,
+    android_ycbcr *ycbcr) {
+  return _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handlejRKNS_4RectEP13android_ycbcr(
+      thisptr, handle, static_cast<uint32_t>(usage), bounds, ycbcr);
+}
+}
+
 }; // namespace android
