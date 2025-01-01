f8d5079de91ea343fcc272a74c046bbfe933dff0
R0rt1z2 <me@r0rt1z2.com>
2025-01-01 18:02:38 +0100

    amazon: init: Use conditional inclusion
    Change-Id: I9977bc495945102db5bacc3cb6222390a7f97ce2

--- init/Android.mk
+++ init/Android.mk
@@ -7,11 +7,17 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -std=c99 -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_amazon.cpp init_amonet.cpp
+
 ifneq ($(TARGET_LIBINIT_DEFINES_FILE),)
   LOCAL_SRC_FILES += ../../../$(TARGET_LIBINIT_DEFINES_FILE)
 else
-  LOCAL_SRC_FILES += init_$(TARGET_BOARD_PLATFORM).cpp
+  ifneq ($(TARGET_BOARD_PLATFORM),)
+    ifeq ($(wildcard $(LOCAL_PATH)/init_$(TARGET_BOARD_PLATFORM).cpp),$(LOCAL_PATH)/init_$(TARGET_BOARD_PLATFORM).cpp)
+      LOCAL_SRC_FILES += init_$(TARGET_BOARD_PLATFORM).cpp
+    endif
+  endif
 endif
+
 LOCAL_MODULE := libinit_amazon
 include $(BUILD_STATIC_LIBRARY)
 
