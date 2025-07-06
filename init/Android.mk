cac0a0da5a4adf7b51970e9d0b01a5dc4942de97
Marie Janssen <jamuraa@google.com>
2025-07-07 00:40:34 +0200

    amazon: init: Don't use `-std=c99` to compile
    Using -std=c99 to compile breaks when we are using C++ files, which
    metrics uses for protobufs.
    
    Bug: 27077250
    Change-Id: Ie94e3ea5d8b219671329108e7795cdc02ecc38b4

--- init/Android.mk
+++ init/Android.mk
@@ -5,7 +5,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
-LOCAL_CFLAGS := -std=c99 -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
+LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_amazon.cpp init_amonet.cpp
 
 ifneq ($(TARGET_LIBINIT_DEFINES_FILE),)
