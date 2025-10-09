5d6b8169598bbc678774b622ac9989809e245003
bengris32 <bengris32@protonmail.ch>
2025-10-09 15:45:14 +0100

    vndk: Provide a v28 copy of `android.hardware.audio.common@4.0-util`
    Change-Id: I3bdc1cdcdd26e0ae4e0f339a69c1a5e74ba77aac
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,6 +1,19 @@
 LOCAL_PATH := prebuilts/vndk
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.audio.common@4.0-util-v28
+LOCAL_MULTILIB := both
+LOCAL_SRC_FILES_arm := v28/arm64/arch-arm-armv8-a/shared/vndk-core/android.hardware.audio.common@4.0-util.so
+LOCAL_SRC_FILES_arm64 := v28/arm64/arch-arm-armv8-a/shared/vndk-core/android.hardware.audio.common@4.0-util.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TARGET_ARCH := arm arm64
+LOCAL_MODULE_TAGS := optional
+LOCAL_CHECK_ELF_FILES := false
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libcompiler_rt-v29
 LOCAL_MULTILIB := both
 LOCAL_SRC_FILES_arm := v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcompiler_rt.so
